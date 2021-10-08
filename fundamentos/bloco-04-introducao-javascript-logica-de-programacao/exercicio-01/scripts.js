14b52d9b51417fbbd55fde14182c7c3ae8684d3a
Hylea <matheusalmeidaf96@gmail.com>
2021-10-08 16:13:42 -0300

    Completes the requirements for exercise 1.

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/exercicio-01/scripts.js
@@ -0,0 +1,21 @@
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+//console.log(typeof patientId)
+//console.log(typeof isEnrolled)
+//console.log(typeof patientInfo)
+//console.log(typeof patientEmail)
+
+const base = 5
+let height = 8
+
+const area = base * height
+const perimeter = (base * 2) + (height * 2)
+
+console.log(area)
+console.log(perimeter)
