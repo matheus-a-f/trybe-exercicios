8fb5d21cb73f70df36dc117a948bd87eed266bd6
Hylea <matheusalmeidaf96@gmail.com>
2021-10-12 16:17:14 -0300

    Fulfills the requirements for the fourteenth exercise.

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/exercicio-14/script.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/exercicio-14/script.js
@@ -1,2 +1,19 @@
 const productPrice = 140
 const salePrice = 200
+const tax = 0.2 * productPrice
+const totalCost = productPrice + tax
+const oneSaleGain = salePrice - totalCost
+let bulkSaleGain;
+
+function gainCalculator(totalCost, salePrice, ammount){
+    if (totalCost <= 0 || salePrice <= 0 || ammount < 0){
+        console.log('Invalid parameter passed.')
+    }
+    else{
+        bulkSaleGain = (salePrice - totalCost) * ammount
+        return bulkSaleGain
+    }
+    
+}
+
+console.log(gainCalculator(totalCost, salePrice, 1000))
