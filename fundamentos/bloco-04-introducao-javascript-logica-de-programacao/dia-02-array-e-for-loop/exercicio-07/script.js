f52828c2e3d10cf03c77997caa6f0e2b799f4456
Hylea <matheusalmeidaf96@gmail.com>
2021-10-13 22:43:57 -0300

    Fulfills the requirements for the eigth exercise.

--- fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-e-for-loop/exercicio-07/script.js
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-02-array-e-for-loop/exercicio-07/script.js
@@ -3,6 +3,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 function arithMean(array){
     let sum = 0
     let mean;
+    let message = ''
 
     for (let i = 0; i < array.length; i++){
         sum += array[i]
@@ -10,7 +11,14 @@ function arithMean(array){
 
     mean = sum / array.length
 
-    return mean
+    if (mean < 20){
+        message = 'Value smaller than 20:'
+    }
+    else{
+        message = 'Value greater than 20:'
+    }
+
+    return message + mean
 }
 
 console.log(arithMean(numbers))
