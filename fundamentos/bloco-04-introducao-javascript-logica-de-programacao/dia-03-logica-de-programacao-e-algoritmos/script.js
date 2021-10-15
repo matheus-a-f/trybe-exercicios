ebaf142cf1aaac1662486c4baea5fa3d9acf7f8e
Hylea <matheusalmeidaf96@gmail.com>
2021-10-14 22:16:17 -0300

    Checkpoint.

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/script.js
@@ -0,0 +1,12 @@
+function squareMaker(size){
+    let output = ''
+
+    for (let x = 0; x < size; x++){
+        for (let y = 0; y < size; y++){
+            console.log('*')
+        }
+        console.log('')
+    }
+}
+
+console.log(squareMaker(5))
