57d5105f0320a3f2003f665656bfb6b2f40846f0
Hylea <matheusalmeidaf96@gmail.com>
2021-10-15 02:02:30 -0300

    Checkpoint

---
+++ fundamentos/bloco-04-introducao-javascript-logica-de-programacao/dia-03-logica-de-programacao-e-algoritmos/exercicio-03/script.js
@@ -0,0 +1,17 @@
+// function triangleMaker(size){
+//     let output = ''
+ 
+//     for (let x = 0; x <= size; x++){
+//         for (let y = 0; y < x; y++){
+//             output += '*'
+//         }
+//         output += '\n'
+//     }
+//     return output
+// }
+
+function reverseTriangle(size){
+    let output = ''
+
+    
+}
