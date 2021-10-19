fe39f1fb0b8808cd809074a09452703d5ae92d8c
Hylea <matheusalmeidaf96@gmail.com>
2021-10-19 18:47:50 -0300

    Fulfills the requirements for the first exercise.

---
+++ fundamentos/bloco-05-DOM-e-seletores/exercicio-01/script.js
@@ -0,0 +1,15 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+const title = document.getElementById('page-title')
+title.innerText = 'Veneno Para Las Hadas'
+
+const secondParagraph = document.getElementById('second-paragraph')
+secondParagraph.innerText = '"Hylea prefers the worship of two children singing over a thousand petitioners silently praying for victory."'
+secondParagraph.style.backgroundColor = 'pink'
+secondParagraph.style.fontStyle = 'italic'
+secondParagraph.style.height = '90px'
+secondParagraph.style.fontSize = '25px'
+
+const subtitleEl = document.getElementById('subtitle')
+subtitleEl.innerText = "Hi, I'm Hylea!"
