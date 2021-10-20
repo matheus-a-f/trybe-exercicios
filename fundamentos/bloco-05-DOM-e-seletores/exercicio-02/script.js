60fda686fce0cae87620fd8b527f586afe277624
Hylea <matheusalmeidaf96@gmail.com>
2021-10-20 13:45:54 -0300

    Fulfills the requirements for the second exercise.

--- fundamentos/bloco-05-DOM-e-seletores/exercicio-02/script.js
+++ fundamentos/bloco-05-DOM-e-seletores/exercicio-02/script.js
@@ -4,18 +4,28 @@ mainHeaderEl.style.backgroundColor = 'darkolivegreen'
 
 //emergency tasks div background color
 const emergencyTasksEl = document.querySelectorAll('.emergency-tasks div')
-for (let i in emergencyTasksEl){
+for (let i = 0; i < emergencyTasksEl.length; i++){
     emergencyTasksEl[i].style.backgroundColor = 'salmon'
 }
 
 //emergency tasks header background color
 const emergencyHeadersEl = document.querySelectorAll('.emergency-tasks h3')
-for(let i in emergencyHeadersEl){
-    emergencyHeadersEl[i].style.backgroundColor = 'black'
+for(let i = 0; i < emergencyHeadersEl.length; i++){
+    emergencyHeadersEl[i].style.backgroundColor = 'darkred'
 }
 
 //non-emergency tasks div background color
 const nonEmergencyTasksEl = document.querySelectorAll('.no-emergency-tasks div')
-for(let i in nonEmergencyTasksEl){
+for(let i = 0; i < nonEmergencyTasksEl.length; i++){
     nonEmergencyTasksEl[i].style.backgroundColor = 'lightskyblue'
 }
+
+//non-emergency tasks header background color
+const nonEmergencyHeadersEl = document.querySelectorAll('.no-emergency-tasks h3')
+for (let i = 0; i < nonEmergencyHeadersEl.length; i++){
+    nonEmergencyHeadersEl[i].style.backgroundColor = 'darkslategrey'
+}
+
+//footer background color
+const footerEl = document.querySelector('#footer-container div')
+footerEl.style.backgroundColor = 'darkolivegreen'
